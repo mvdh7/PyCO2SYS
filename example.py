cb8e77b358514e3e325b292290201db889a9d0d4
Matthew Humphreys <mvdh7@users.noreply.github.com>
2020-01-31 16:03:51 +0100

    Continued testing

---
+++ example.py
@@ -0,0 +1,9 @@
+import numpy as np
+from PyCO2SYS import CO2SYS
+
+ta = np.vstack(np.ones(10)*2200)
+dic = np.ones(10)*2050
+DICT, DATA, HEADERS, NICEHEADERS = CO2SYS(ta, dic, 1, 2, 35, 10, 10, 0, 0, 0, 0, 3, 10, 3)
+# print(test)
+
+# PyCO2SYS._Constants(298.15, 0)
