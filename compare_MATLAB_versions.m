19d394e6bd08d046a09526f02eb746c88612ec20
Matthew Humphreys <mvdh7@users.noreply.github.com>
2020-03-19 09:31:51 +0100

    Extract input generator into a function

--- compare_MATLAB_versions.m
+++ compare_MATLAB_versions.m
@@ -1,30 +1,14 @@
 %% Set up input conditions
-setPARS = [2300 2000 8.05 400 405];
-PAR12combos_raw = combnk(1:numel(setPARS), 2);
-pHscales_opts = 1:4;
-K1K2_opts = 1:14;
-KSO4_only_opts = 1:4;
-KF_opts = 1;
-nopts = numel(pHscales_opts)*numel(K1K2_opts)*numel(KSO4_only_opts)* ...
-    numel(KF_opts);
-PAR12combos_raw = PAR12combos_raw( ...
-    ~(PAR12combos_raw(:, 1) == 4 & PAR12combos_raw(:, 2) == 5), :);
-PAR12combos_raw = [PAR12combos_raw; fliplr(PAR12combos_raw)];
-ncombos = size(PAR12combos_raw, 1);
-PAR12combos = repmat(PAR12combos_raw, nopts, 1);
-PARSin = setPARS(PAR12combos);
-[pHscales, K1K2, KSO4_only, KF] = ...
-    ndgrid(pHscales_opts, K1K2_opts, KSO4_only_opts, KF_opts);
-pHscales = repmat(pHscales(:), 1, ncombos)';
-K1K2 = repmat(K1K2(:), 1, ncombos)';
-KSO4_only = repmat(KSO4_only(:), 1, ncombos)';
-KF = repmat(KF(:), 1, ncombos)';
-pHscales = pHscales(:);
-K1K2 = K1K2(:);
-KSO4_only = KSO4_only(:);
-KF = KF(:);
-sal = 33.1*ones(size(K1K2));
-sal(K1K2 == 8) = 0;
+PARvalues = [2300 2000 8.05 400 405];
+PARTYPEs = 1:5;
+pHSCALEIN_opts = 1:4;
+K1K2CONSTANTS_opts = 1:14;
+KSO4CONSTANTS_opts = 1:4;
+KFCONSTANT_opts = 1;
+SALvalue = 33.1;
+[P1, P2, P1type, P2type, sal, pHscales, K1K2, KSO4_only, KSO4, KF, ...
+    BSal] = CO2SYSigen(PARvalues, PARTYPEs, SALvalue, pHSCALEIN_opts, ...
+    K1K2CONSTANTS_opts, KSO4CONSTANTS_opts, KFCONSTANT_opts);
 
 % Define single value inputs
 tempin = 24;
@@ -35,31 +19,21 @@ si = 10;
 phos = 2;
 nh3 = 0;
 h2s = 0;
-P1 = PARSin(:, 1);
-P2 = PARSin(:, 2);
-P1type = PAR12combos(:, 1);
-P2type = PAR12combos(:, 2);
-
-% Convert old combined KSO4 & BSal input to separate variables
-only2KSO4 = [1 2 1 2];
-only2BSal = [1 1 2 2];
-KSO4 = only2KSO4(KSO4_only)';
-BSal = only2BSal(KSO4_only)';
 
 % Run CO2SYS
 disp('Running CO2SYS v2.0.5:')
 tic
-[data_v2, headers_v2] = CO2SYSv2(P1, P2, P1type, P2type, sal, ...
+[data_v2, headers_v2] = CO2SYSv2_0_5(P1, P2, P1type, P2type, sal, ...
     tempin, tempout, presin, presout, si, phos, pHscales, K1K2, KSO4_only);
 toc
 disp('Running CO2SYS v1.21:')
 tic
-[data_v121, headers_v121] = CO2SYS_v1_21(P1, P2, P1type, P2type, sal, ...
+[data_v121, headers_v121] = CO2SYSv1_21(P1, P2, P1type, P2type, sal, ...
     tempin, tempout, presin, presout, si, phos, nh3, h2s, pHscales, ...
     K1K2, KSO4, KF, BSal);
 toc
 
-% Convert output to tables
+% Convert outputs to tables
 clear v2 v121
 for V = 1:numel(headers_v2)
     v2.(headers_v2{V}) = data_v2(:, V);
--- compare_MATLABv2_0_5.m
+++ compare_MATLABv2_0_5.m
@@ -1,18 +1,14 @@
 %% Set up input conditions
-PAR12combos = repmat(combnk(1:5, 2), 240, 1);
-PAR12combos = PAR12combos( ...
-    ~(PAR12combos(:, 1) == 4 & PAR12combos(:, 2) == 5), :);
-setPARS = [2250 2100 8.1 400 405];
-PARSin = setPARS(PAR12combos);
-[pHscales, K1K2, KSO4] = ndgrid(1:4, 1:15, 1:4);
-pHscales = repmat(pHscales(:), 1, 9)';
-K1K2 = repmat(K1K2(:), 1, 9)';
-KSO4 = repmat(KSO4(:), 1, 9)';
-pHscales = pHscales(:);
-K1K2 = K1K2(:);
-KSO4 = KSO4(:);
-sal = 33.1*ones(size(K1K2));
-sal(K1K2 == 8) = 0;
+PARvalues = [2250 2100 8.1 400 405];
+PARTYPEs = 1:5;
+pHSCALEIN_opts = 1:4;
+K1K2CONSTANTS_opts = 1:15;
+KSO4CONSTANTS_opts = 1:4;
+KFCONSTANT_opts = 1;
+SALvalue = 33.1;
+[P1, P2, P1type, P2type, sal, pHscales, K1K2, KSO4_only, KSO4, KF, ...
+    BSal] = CO2SYSigen(PARvalues, PARTYPEs, SALvalue, pHSCALEIN_opts, ...
+    K1K2CONSTANTS_opts, KSO4CONSTANTS_opts, KFCONSTANT_opts);
 tempin = 24;
 tempout = 12;
 presin = 1;
@@ -23,15 +19,16 @@ phos = 1;
 %% Run CO2SYS
 tic
 [DATA, HEADERS] = ...
-    CO2SYSv2_0_5(PARSin(:, 1), PARSin(:, 2), PAR12combos(:, 1), ...
-    PAR12combos(:, 2), sal, tempin, tempout, presin, presout, ...
-    si, phos, pHscales, K1K2, KSO4);
+    CO2SYSv2_0_5(P1, P2, P1type, P2type, sal, tempin, tempout, presin, ...
+    presout, si, phos, pHscales, K1K2, KSO4);
 toc
 
 %% Extract and save outputs
 for V = 1:numel(HEADERS)
     co2s.(HEADERS{V}) = DATA(:, V);
 end % for V
-co2s.PARSin = PARSin;
-co2s.PAR12combos = PAR12combos;
+co2s.PAR1 = P1;
+co2s.PAR2 = P2;
+co2s.PAR1TYPE = P1type;
+co2s.PAR2TYPE = P2type;
 save('compare/MATLAB_CO2SYSv2_0_5.mat', 'co2s')
