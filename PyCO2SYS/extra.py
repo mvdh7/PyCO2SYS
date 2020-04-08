01de1354fe5099b7624b04677f1aa843dc90ee1b
mvdh7 <m.p.humphreys@icloud.com>
2020-04-08 11:10:30 +0200

    Add isocap functions

--- PyCO2SYS/extra.py
+++ PyCO2SYS/extra.py
@@ -18,3 +18,18 @@ def buffers_ESM10(TC, TA, CO2, HCO3, CO3, pH, OH, BAlk, KB):
     omegaTA = AC - TC*Q/P # corrected as for omegaTC (RAH18), HCO3 => Q
     ## omegaTA = AC - TC*HCO3/P # original ESM10 equation, WRONG
     return gammaTC, betaTC, omegaTC, gammaTA, betaTA, omegaTA
+
+def bgc_isocap(CO2, pH, K1, K2, KB, KW, TB):
+    """Isocapnic quotient of HDW18."""
+    h = 10.0**-pH
+    return ((K1*CO2*h + 4*K1*K2*CO2 + KW*h + h**3)*(KB + h)**2 +
+        KB*TB*h**3)/(K1*CO2*(2*K2 + h)*(KB + h)**2)
+
+def bgc_isocap_approx(TC, pCO2, K0, K1, K2):
+    """Approximate isocapnic quotient of HDW18."""
+    return 1 + 2*(K2/(K0*K1))*TC/pCO2
+
+def psi(CO2, pH, K1, K2, KB, KW, TB):
+    """Psi of F94."""
+    Q = bgc_isocap(CO2, pH, K1, K2, KB, KW, TB)
+    return -1 + 2/Q
