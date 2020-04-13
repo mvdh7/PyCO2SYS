ec97a06e4876299b248e7ba7f4cbdbaf4e318f48
Matthew Humphreys <m.humphs@gmail.com>
2020-04-13 23:09:44 +0200

    Try jax

--- gradtest.py
+++ gradtest.py
@@ -1,9 +1,9 @@
 from autograd import elementwise_grad as egrad
-from autograd import numpy as np
+from jax import numpy as np
 from jax import grad, vmap
 
 def whoop(varin):
-    varsum = np.zeros(np.size(varin))
+    varsum = np.sqrt(varin)
     i = 0
     while i < 10:
         varsum = varsum + varin + 0.5
@@ -12,5 +12,12 @@ def whoop(varin):
 
 varin = np.arange(10.0)    
 varout = whoop(varin)
-gradout = egrad(whoop)(varin)
-jaxout = vmap(grad(whoop), in_axes=(0,))(varin)
+# gradout = egrad(whoop)(varin)
+jaxout = grad(whoop)(varin)
+
+def sum_logistic(x):
+  return np.sum(1.0 / (1.0 + np.exp(-x)))
+
+x_small = np.arange(3.)
+derivative_fn = grad(sum_logistic)
+print(derivative_fn(x_small))
