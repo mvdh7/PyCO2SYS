43d572f731e6a1a2ed026a6bd5011b9d05221e30
Matthew Humphreys <m.p.humphreys@icloud.com>
2020-04-24 12:17:59 +0200

    Black style

--- compare/compare_MATLABv2_0_5.py
+++ compare/compare_MATLABv2_0_5.py
@@ -4,21 +4,37 @@ import PyCO2SYS as pyco2
 
 # Import input conditions: "compare_MATLABv2_0_5.csv" was generated in MATLAB
 # using "compare_MATLABv2_0_5.m".
-co2matlab = pd.read_csv('compare/data/compare_MATLABv2_0_5.csv')
+co2matlab = pd.read_csv("compare/data/compare_MATLABv2_0_5.csv")
 
 # Run PyCO2SYS.CO2SYS under the same conditions
-co2inputs = [co2matlab[var].values for var in ['PAR1', 'PAR2', 'PAR1TYPE', 'PAR2TYPE',
-    'SAL', 'TEMPIN', 'TEMPOUT', 'PRESIN', 'PRESOUT', 'SI', 'PO4', 'pHSCALEIN',
-    'K1K2CONSTANTS', 'KSO4CONSTANTS']]
+co2inputs = [
+    co2matlab[var].values
+    for var in [
+        "PAR1",
+        "PAR2",
+        "PAR1TYPE",
+        "PAR2TYPE",
+        "SAL",
+        "TEMPIN",
+        "TEMPOUT",
+        "PRESIN",
+        "PRESOUT",
+        "SI",
+        "PO4",
+        "pHSCALEIN",
+        "K1K2CONSTANTS",
+        "KSO4CONSTANTS",
+    ]
+]
 go = time()
 co2py = pyco2.CO2SYS(*co2inputs)
-print('PyCO2SYS runtime = {:.6f} s'.format(time() - go))
+print("PyCO2SYS runtime = {:.6f} s".format(time() - go))
 co2py = pd.DataFrame(co2py)
 
 # Also test the original CO2SYS clone
 go = time()
 DATA, HEADERS, _ = pyco2.original.CO2SYS(*co2inputs)
-print('PyCO2SYS.original runtime = {:.6f} s'.format(time() - go))
+print("PyCO2SYS.original runtime = {:.6f} s".format(time() - go))
 co2pyo = pd.DataFrame({header: DATA[:, h] for h, header in enumerate(HEADERS)})
 
 # Compare the results
