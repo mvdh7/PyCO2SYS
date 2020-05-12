f4d50b0e05cffa90ff5fe30d401ea3f84bac59f0
Matthew Humphreys <m.p.humphreys@icloud.com>
2020-05-12 11:22:21 +0200

    Begin core function

--- PyCO2SYS/uncertainty.py
+++ PyCO2SYS/uncertainty.py
@@ -1,4 +1,7 @@
 # PyCO2SYS: marine carbonate system calculations in Python.
 # Copyright (C) 2020  Matthew Paul Humphreys et al.  (GNU GPLv3)
 """Uncertainty propagation."""
-    
+
+
+def core(par1type, par2type):
+    return
