b2069a49f1a7016931c20ae47e179e67a0221cf0
Matthew Humphreys <m.p.humphreys@icloud.com>
2020-05-12 16:11:26 +0200

    Add pH grads

--- PyCO2SYS/uncertainty.py
+++ PyCO2SYS/uncertainty.py
@@ -99,6 +99,31 @@ def dcore_dparX__parY(parXtype, parYtype, TA, TC, PH, FC, CARB, HCO3, totals, Ks
         dPH_dTC__HCO3 = 1 / dTC_dPH__HCO3
         dFC_dTC__HCO3 = dFC_dPH__HCO3 / dTC_dPH__HCO3
         dCARB_dTC__HCO3 = dCARB_dPH__HCO3 / dTC_dPH__HCO3
+    if np_any((parXtype == 3) & (parYtype == 1)):  # dvar_dPH__TA
+        dTC_dPH__TA = egrad(lambda PH: get.TCfromTApH(TA, PH, totals, Ks))(PH)
+        dFC_dPH__TA = egrad(lambda PH: get.fCO2fromTApH(TA, PH, totals, Ks))(PH)
+        dCARB_dPH__TA = egrad(lambda PH: get.CarbfromTApH(TA, PH, totals, Ks))(PH)
+        dHCO3_dPH__TA = egrad(lambda PH: get.HCO3fromTApH(TA, PH, totals, Ks))(PH)
+    if np_any((parXtype == 3) & (parYtype == 2)):  # dvar_dPH__TC
+        dTA_dPH__TC = egrad(lambda PH: get.TAfromTCpH(TC, PH, totals, Ks))(PH)
+        dFC_dPH__TC = egrad(lambda PH: get.fCO2fromTCpH(TC, PH, K0, K1, K2))(PH)
+        dCARB_dPH__TC = egrad(lambda PH: get.CarbfromTCpH(TC, PH, K1, K2))(PH)
+        dHCO3_dPH__TC = egrad(lambda PH: get.HCO3fromTCpH(TC, PH, K1, K2))(PH)
+    if np_any((parXtype == 3) & isin(parYtype, [4, 5, 8])):  # dvar_dPH__FC
+        dTA_dPH__FC = egrad(lambda PH: get.TAfrompHfCO2(PH, FC, totals, Ks))(PH)
+        dTC_dPH__FC = egrad(lambda PH: get.TCfrompHfCO2(PH, FC, K0, K1, K2))(PH)
+        dCARB_dPH__FC = egrad(lambda PH: get.CarbfrompHfCO2(PH, FC, K0, K1, K2))(PH)
+        dHCO3_dPH__FC = egrad(lambda PH: get.HCO3frompHfCO2(PH, FC, K0, K1))(PH)
+    if np_any((parXtype == 3) & (parYtype == 6)):  # dvar_dPH__CARB
+        dTC_dPH__CARB = egrad(lambda PH: get.TCfrompHCarb(PH, CARB, K1, K2))(PH)
+        dTA_dPH__CARB = egrad(lambda PH: get.TAfrompHCarb(PH, CARB, totals, Ks))(PH)
+        dFC_dPH__CARB = egrad(lambda PH: get.fCO2frompHCarb(PH, CARB, K0, K1, K2))(PH)
+        dHCO3_dPH__CARB = egrad(lambda PH: get.HCO3frompHCarb(PH, CARB, K2))(PH)
+    if np_any((parXtype == 3) & (parYtype == 7)):  # dvar_dPH__HCO3
+        dTC_dPH__HCO3 = egrad(lambda PH: get.TCfrompHHCO3(PH, HCO3, K1, K2))(PH)
+        dTA_dPH__HCO3 = egrad(lambda PH: get.TAfrompHHCO3(PH, HCO3, totals, Ks))(PH)
+        dFC_dPH__HCO3 = egrad(lambda PH: get.fCO2frompHHCO3(PH, HCO3, K0, K1))(PH)
+        dCARB_dPH__HCO3 = egrad(lambda PH: get.CarbfrompHHCO3(PH, HCO3, K2))(PH)
     # Preallocate empty arrays for derivatives
     dTA_dX__Y = full(size(parXtype), nan)
     dTC_dX__Y = full(size(parXtype), nan)
@@ -182,6 +207,44 @@ def dcore_dparX__parY(parXtype, parYtype, TA, TC, PH, FC, CARB, HCO3, totals, Ks
             dPH_dX__Y = where(XY, dPH_dTC__HCO3, dPH_dX__Y)
             dFC_dX__Y = where(XY, dFC_dTC__HCO3, dFC_dX__Y)
             dCARB_dX__Y = where(XY, dCARB_dTC__HCO3, dCARB_dX__Y)
+            dHCO3_dX__Y = where(XY, 0.0, dHCO3_dX__Y)
+    X = parXtype == 3  # PH - seawater pH
+    if np_any(X):
+        dPH_dX__Y = where(X, 1.0, dPH_dX__Y)
+        XY = X & (parYtype == 1)  # PH, TA
+        if np_any(XY):
+            dTA_dX__Y = where(XY, 0.0, dTA_dX__Y)
+            dTC_dX__Y = where(XY, dTC_dPH__TA, dTC_dX__Y)
+            dFC_dX__Y = where(XY, dFC_dPH__TA, dFC_dX__Y)
+            dCARB_dX__Y = where(XY, dCARB_dPH__TA, dCARB_dX__Y)
+            dHCO3_dX__Y = where(XY, dHCO3_dPH__TA, dHCO3_dX__Y)
+        XY = X & (parYtype == 2)  # PH, TC
+        if np_any(XY):
+            dTA_dX__Y = where(XY, dTA_dPH__TC, dTA_dX__Y)
+            dTC_dX__Y = where(XY, 0.0, dTC_dX__Y)
+            dFC_dX__Y = where(XY, dFC_dPH__TC, dFC_dX__Y)
+            dCARB_dX__Y = where(XY, dCARB_dPH__TC, dCARB_dX__Y)
+            dHCO3_dX__Y = where(XY, dHCO3_dPH__TC, dHCO3_dX__Y)
+        XY = X & isin(parYtype, [4, 5, 8])  # PH, (PC | FC | CO2)
+        if np_any(XY):
+            dTA_dX__Y = where(XY, dTA_dPH__FC, dTA_dX__Y)
+            dTC_dX__Y = where(XY, dTC_dPH__FC, dTC_dX__Y)
+            dFC_dX__Y = where(XY, 0.0, dFC_dX__Y)
+            dCARB_dX__Y = where(XY, dCARB_dPH__FC, dCARB_dX__Y)
+            dHCO3_dX__Y = where(XY, dHCO3_dPH__FC, dHCO3_dX__Y)
+        XY = X & (parYtype == 6)  # PH, CARB
+        if np_any(XY):
+            dTA_dX__Y = where(XY, dTA_dPH__CARB, dTA_dX__Y)
+            dTC_dX__Y = where(XY, dTC_dPH__CARB, dTC_dX__Y)
+            dFC_dX__Y = where(XY, dFC_dPH__CARB, dFC_dX__Y)
+            dCARB_dX__Y = where(XY, 0.0, dCARB_dX__Y)
+            dHCO3_dX__Y = where(XY, dHCO3_dPH__CARB, dHCO3_dX__Y)
+        XY = X & (parYtype == 7)  # PH, HCO3
+        if np_any(XY):
+            dTA_dX__Y = where(XY, dTA_dPH__HCO3, dTA_dX__Y)
+            dTC_dX__Y = where(XY, dTC_dPH__HCO3, dTC_dX__Y)
+            dFC_dX__Y = where(XY, dFC_dPH__HCO3, dFC_dX__Y)
+            dCARB_dX__Y = where(XY, dCARB_dPH__HCO3, dCARB_dX__Y)
             dHCO3_dX__Y = where(XY, 0.0, dHCO3_dX__Y)
     # Get pCO2 and CO2(aq) derivatives from fCO2
     dPC_dX__Y = dFC_dX__Y / Ks["FugFac"]
--- tests/test_uncertainty.py
+++ tests/test_uncertainty.py
@@ -189,4 +189,91 @@ def test_parX_TC():
     assert close_enough(dCO2_uncert, dCO2_direct)
 
 
-test_parX_TC()
+def test_parX_PH():
+    """Compare derivatives from uncertainties module vs direct grads of solve.fill."""
+    # Define MCS parameters
+    parY = np.array([2300e-6, 2100e-6, 400e-6, 400e-6, 350e-6, 1800e-6, 10e-6])
+    parYtype = np.array([1, 2, 4, 5, 6, 7, 8])
+    npts = np.size(parY)
+    parX = np.full(npts, 8.1)
+    parXtype = np.full(npts, 3)
+    # Set and get total molinities
+    Sal = np.full(npts, 31.0)
+    TSi = np.full(npts, 12.0)
+    TPO4 = np.full(npts, 1.5)
+    TNH3 = np.full(npts, 2.0)
+    TH2S = np.full(npts, 0.5)
+    WhichKs = np.full(npts, 10)
+    WhoseTB = np.full(npts, 2)
+    totals = pyco2.salts.assemble(Sal, TSi, TPO4, TNH3, TH2S, WhichKs, WhoseTB)
+    # Set and get equilibrium constants
+    TempC = np.full(npts, 22.3)
+    Pdbar = np.full(npts, 100.0)
+    pHScale = np.full(npts, 1)
+    WhoseKSO4 = np.full(npts, 1)
+    WhoseKF = np.full(npts, 1)
+    Ks = pyco2.equilibria.assemble(
+        TempC, Pdbar, Sal, totals, pHScale, WhichKs, WhoseKSO4, WhoseKF
+    )
+    # Expand and solve MCS parameters
+    Icase = pyco2.solve.getIcase(parXtype, parYtype, checks=True)
+    TA, TC, PH, PC, FC, CARB, HCO3, CO2 = pyco2.solve.pair2core(
+        parX, parY, parXtype, parYtype, convert_units=False, checks=True
+    )
+    TA, TC, PH, PC, FC, CARB, HCO3, CO2 = pyco2.solve.fill(
+        Icase, TA, TC, PH, PC, FC, CARB, HCO3, CO2, totals, Ks
+    )
+    # Get uncertainty derivatives with the uncertainty module
+    go = time()
+    (
+        dTA_uncert,
+        dTC_uncert,
+        dPH_uncert,
+        dPC_uncert,
+        dFC_uncert,
+        dCARB_uncert,
+        dHCO3_uncert,
+        dCO2_uncert,
+    ) = pyco2.uncertainty.dcore_dparX__parY(
+        parXtype, parYtype, TA, TC, PH, FC, CARB, HCO3, totals, Ks
+    )
+    print("  Uncertainty module runtime = {:.5f} s".format(time() - go))
+    # Get corresponding uncertainty derivatives directly
+    Icase = pyco2.solve.getIcase(parXtype, parYtype)
+
+    def _dPH_direct(i):
+        return egrad(
+            lambda PH: pyco2.solve.fill(
+                Icase, TA, TC, PH, PC, FC, CARB, HCO3, CO2, totals, Ks
+            )[i]
+        )(PH)
+
+    go = time()
+    dTA_direct = _dPH_direct(0)
+    dTC_direct = _dPH_direct(1)
+    dPH_direct = _dPH_direct(2)
+    dPC_direct = _dPH_direct(3)
+    dFC_direct = _dPH_direct(4)
+    dCARB_direct = _dPH_direct(5)
+    dHCO3_direct = _dPH_direct(6)
+    dCO2_direct = _dPH_direct(7)
+    print("Direct uncertainties runtime = {:.5f} s".format(time() - go))
+    # Make sure they agree
+    # print(np.array([dTA_uncert, dTA_direct]))
+    # print(np.array([dTC_uncert, dTC_direct]))
+    # print(np.array([dPH_uncert, dPH_direct]))
+    # print(np.array([dPC_uncert, dPC_direct]))
+    # print(np.array([dFC_uncert, dFC_direct]))
+    # print(np.array([dCARB_uncert, dCARB_direct]))
+    # print(np.array([dHCO3_uncert, dHCO3_direct]))
+    # print(np.array([dCO2_uncert, dCO2_direct]))
+    assert close_enough(dTA_uncert, dTA_direct)
+    assert close_enough(dTC_uncert, dTC_direct)
+    assert close_enough(dPH_uncert, dPH_direct)
+    assert close_enough(dPC_uncert, dPC_direct)
+    assert close_enough(dFC_uncert, dFC_direct)
+    assert close_enough(dCARB_uncert, dCARB_direct)
+    assert close_enough(dHCO3_uncert, dHCO3_direct)
+    assert close_enough(dCO2_uncert, dCO2_direct)
+
+test_parX_PH()
