fe3a7b8176959fa0d1ec1f25a3e20c0cabce52ea
Matthew Humphreys <m.p.humphreys@icloud.com>
2020-07-28 11:21:40 +0200

    Fix weird test error

--- tests/test_nd.py
+++ tests/test_nd.py
@@ -1,24 +1,24 @@
+import numpy as np
 import PyCO2SYS as pyco2
-from autograd import numpy as np
 
 # Set up for basic conditioning check
-par1 = np.random.normal(size=(5, 4, 3))
-par2 = [10, 11, 12]
-par3 = np.random.normal(size=(5, 4, 3))
-par4 = np.random.normal(size=(3, 4, 5))
-par5 = 12.2
-par6 = "test"
-par7 = np.random.normal(size=(5, 1, 3))
-par8 = ["test"]
+par1c = np.random.normal(size=(5, 4, 3))
+par2c = [10, 11, 12]
+par3c = np.random.normal(size=(5, 4, 3))
+par4c = np.random.normal(size=(3, 4, 5))
+par5c = 12.2
+par6c = "test"
+par7c = np.random.normal(size=(5, 1, 3))
+par8c = ["test"]
 inputs = {
-    "PAR1": par1,
-    "PAR2": par2,
-    "par3": par3,
-    "par4": par4,  # this one should make it fail
-    "par5": par5,
-    "par6": par6,
-    "par7": par7,
-    "par8": par8,
+    "PAR1": par1c,
+    "PAR2": par2c,
+    "par3": par3c,
+    "par4": par4c,  # this one should make it fail
+    "par5": par5c,
+    "par6": par6c,
+    "par7": par7c,
+    "par8": par8c,
 }
 inputs_no_4 = {k: v for k, v in inputs.items() if k != "par4"}
 
@@ -52,7 +52,7 @@ kwargs = {
     "k_carbonic_1": [1e-6, 1.1e-6, 1.2e-6],
     "temperature_out": 0,
 }
-co2nd = pyco2.engine.nd.CO2SYS(par1, par2, par1_type, par2_type, **kwargs)
+co2nd = pyco2.CO2SYS_nd(par1, par2, par1_type, par2_type, **kwargs)
 
 
 def test_nd_misc():
@@ -79,18 +79,18 @@ test_scalars()
 
 
 # Test with TA/DIC grid
-par1 = np.linspace(2100, 2400, 11)
-par2 = np.vstack(np.linspace(2000, 2300, 11))
-par1_type = 1
-par2_type = 2
-co2nd_grid = pyco2.CO2SYS_nd(par1, par2, par1_type, par2_type)
+par1g = np.linspace(2100, 2400, 11)
+par2g = np.vstack(np.linspace(2000, 2300, 11))
+par1g_type = 1
+par2g_type = 2
+co2nd_grid = pyco2.CO2SYS_nd(par1g, par2g, par1g_type, par2g_type)
 
 
 def test_grid():
     assert (
         np.shape(co2nd_grid["alkalinity"])
         == np.shape(co2nd_grid["dic"])
-        == np.broadcast(par1, par2).shape
+        == np.broadcast(par1g, par2g).shape
     )
 
 
