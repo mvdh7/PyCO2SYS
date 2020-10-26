03a68c4b932f4149054a0fc495dabb49419dda62
Matthew Humphreys <m.p.humphreys@icloud.com>
2020-10-26 12:04:11 +0100

    Add ammonia test

--- tests/test_extra_components.py
+++ tests/test_extra_components.py
@@ -44,6 +44,12 @@ r3 = pyco2.sys(
     opt_pH_scale=opt_pH_scale,
 )
 
+# Add ammonia and alpha-as-ammonia
+total_ammonia = 101.5
+r4_ammonia = pyco2.sys(*args, total_ammonia=total_ammonia, opt_pH_scale=opt_pH_scale)
+r4_alpha = pyco2.sys(*args, total_alpha=total_ammonia, k_alpha=r4_ammonia["k_ammonia"], opt_pH_scale=opt_pH_scale)
+
+
 
 def test_silicate_alpha():
     assert np.all(np.isclose(r0["alkalinity_silicate"], r1["alkalinity_alpha"]))
@@ -60,6 +66,12 @@ def test_alpha_beta():
     assert np.all(np.isclose(r0["alkalinity_borate"], r3["alkalinity_beta"]))
 
 
+def test_ammonia_alpha():
+    assert np.all(np.isclose(r4_ammonia["alkalinity_ammonia"], r4_alpha["alkalinity_alpha"]))
+    assert np.all(np.isclose(r4_ammonia["beta_alk"], r4_alpha["beta_alk"]))
+
+
 test_silicate_alpha()
 test_fluoride_beta()
 test_alpha_beta()
+test_ammonia_alpha()
