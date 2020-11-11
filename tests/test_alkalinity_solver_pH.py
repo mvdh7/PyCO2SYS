da7d338235d0abd2834bb7b532f92dec3ae0a30b
Matthew Humphreys <m.p.humphreys@icloud.com>
2020-11-11 08:25:55 +0100

    Tidy up

--- tests/test_alkalinity_solver_pH.py
+++ tests/test_alkalinity_solver_pH.py
@@ -7,24 +7,21 @@ dic = 2500
 
 # Get all pHs from TA and DIC
 opt_pH_scale = np.array([1, 2, 3, 4])
-results_original = pyco2.sys(alkalinity, dic, 1, 2, opt_pH_scale=opt_pH_scale)
-
-# Get all pHs from TA and DIC, again
-results_fixed = pyco2.sys(alkalinity, dic, 1, 2, opt_pH_scale=np.array([1, 2, 3, 4]))
+results = pyco2.sys(alkalinity, dic, 1, 2, opt_pH_scale=np.array([1, 2, 3, 4]))
 
 # Check they're consistent regardless of input scale
 def test_pH_scale_consistency():
     assert np.all(
-        np.isclose(results_fixed["pH_total"], results_fixed["pH"][opt_pH_scale == 1])
+        np.isclose(results["pH_total"], results["pH"][opt_pH_scale == 1])
     )
     assert np.all(
-        np.isclose(results_fixed["pH_sws"], results_fixed["pH"][opt_pH_scale == 2])
+        np.isclose(results["pH_sws"], results["pH"][opt_pH_scale == 2])
     )
     assert np.all(
-        np.isclose(results_fixed["pH_free"], results_fixed["pH"][opt_pH_scale == 3])
+        np.isclose(results["pH_free"], results["pH"][opt_pH_scale == 3])
     )
     assert np.all(
-        np.isclose(results_fixed["pH_nbs"], results_fixed["pH"][opt_pH_scale == 4])
+        np.isclose(results["pH_nbs"], results["pH"][opt_pH_scale == 4])
     )
 
 
