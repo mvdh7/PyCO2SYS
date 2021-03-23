ad0c90089d669d868d9f69e596d5bea3bd189203
Matthew Humphreys <m.p.humphreys@icloud.com>
2021-03-23 08:58:14 +0100

    Use np.isclose

--- tests/test_alkalinity_solver_pH.py
+++ tests/test_alkalinity_solver_pH.py
@@ -11,18 +11,10 @@ results = pyco2.sys(alkalinity, dic, 1, 2, opt_pH_scale=np.array([1, 2, 3, 4]))
 
 # Check they're consistent regardless of input scale
 def test_pH_scale_consistency():
-    assert np.all(
-        np.isclose(results["pH_total"], results["pH"][opt_pH_scale == 1])
-    )
-    assert np.all(
-        np.isclose(results["pH_sws"], results["pH"][opt_pH_scale == 2])
-    )
-    assert np.all(
-        np.isclose(results["pH_free"], results["pH"][opt_pH_scale == 3])
-    )
-    assert np.all(
-        np.isclose(results["pH_nbs"], results["pH"][opt_pH_scale == 4])
-    )
+    assert np.all(np.isclose(results["pH_total"], results["pH"][opt_pH_scale == 1]))
+    assert np.all(np.isclose(results["pH_sws"], results["pH"][opt_pH_scale == 2]))
+    assert np.all(np.isclose(results["pH_free"], results["pH"][opt_pH_scale == 3]))
+    assert np.all(np.isclose(results["pH_nbs"], results["pH"][opt_pH_scale == 4]))
 
 
 # test_pH_scale_consistency()
