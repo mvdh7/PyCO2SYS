d59e040e1d25ad20cece36d4d4b95a4864bef4d9
Matthew Humphreys <m.p.humphreys@icloud.com>
2021-03-29 13:12:54 +0200

    Update Python versions

--- setup.py
+++ setup.py
@@ -18,9 +18,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Natural Language :: English",
