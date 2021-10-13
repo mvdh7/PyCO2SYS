21f29ba2d3fe6bf867fea1219bd9dc8e7e1c53a7
Matthew P. Humphreys <m.p.humphreys@icloud.com>
2021-10-13 11:18:18 +0200

    Use isclose

--- tests/test_nd_uncertainty.py
+++ tests/test_nd_uncertainty.py
@@ -73,11 +73,11 @@ def test_old_new():
 
 
 def test_uncertainty_old_new():
-    assert np.all(uncert_old["pHin"] - uncertainties["pH"].ravel() == 0)
+    assert np.all(np.isclose(uncert_old["pHin"], uncertainties["pH"].ravel()))
 
 
-# test_old_new()
-# test_uncertainty_old_new()
+test_old_new()
+test_uncertainty_old_new()
 
 # # Try out the standard uncertainties of OEDG18
 # uncertainties_pk, components_pk = pyco2.uncertainty.propagate_nd(
