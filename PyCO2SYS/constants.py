a85e3cd31eaab6a7e263f8b23d9c4ef72a0eb6be
Matthew P. Humphreys <m.p.humphreys@icloud.com>
2022-07-28 09:42:05 +0200

    Add Boltzmann constant

--- PyCO2SYS/constants.py
+++ PyCO2SYS/constants.py
@@ -4,14 +4,13 @@
 
 from autograd import numpy as np
 
-
+# Universal gas constant
 RGasConstant_DOEv2 = 83.1451  # ml bar-1 K-1 mol-1, DOEv2 (always used by default)
 RGasConstant_DOEv3 = 83.14472  # ml bar-1 K-1 mol-1, DOEv3 (never used in PyCO2SYS)
 RGasConstant_CODATA2018 = 83.14462618  # 10^-1 J mol^-1 K^-1 (available from v1.4.1)
 # Source: https://physics.nist.gov/cgi-bin/cuu/Value?r (2018 CODATA)
 # RGasConstant_CODATA2018 added in v1.4.1 for consistency with CO2SYS-MATLAB v3,
 # but the default remains RGasConstant_DOEv2.
-Tzero = 273.15  # 0 degC in K
 
 
 def RGasConstant(WhichR):
@@ -21,3 +20,10 @@ def RGasConstant(WhichR):
     RGas = np.where(WhichR == 2, RGasConstant_DOEv3, RGas)  # DOEv3
     RGas = np.where(WhichR == 3, RGasConstant_CODATA2018, RGas)  # 2018 CODATA
     return RGas
+
+
+# Absolute zero
+Tzero = 273.15  # 0 degC in K
+
+# Boltzmann constant
+k_boltzmann = 1.380649e-23  # m**2 * kg / (s * K)
