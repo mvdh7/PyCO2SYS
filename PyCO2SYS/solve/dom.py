Fix sign error

PyCO2SYS/solve/dom.py
@@ -77,7 +77,7 @@ def charge_balance(log10_chi, c_ions, z_ions, ionic_strength, nd_params):
     """Overall charge balance to solve for log10(chi)."""
     chi = 10.0**log10_chi
     cH = c_ions[:, 0]  # assumes first ion column is always H!
-    return nica_charge(cH, chi, nd_params) - donnan_charge(
+    return nica_charge(cH, chi, nd_params) + donnan_charge(
         chi, c_ions, z_ions, ionic_strength, nd_params
     )
 
@@ -92,7 +92,7 @@ def solve_chi(
 ):
     """Solve charge balance for log10(chi) with Newton-Raphson."""
     if log10_chi_guess is None:
-        log10_chi = np.full_like(c_ions[:, 0], 0.0)  # first guess if needed
+        log10_chi = np.full_like(c_ions[:, 0], 5.0)  # first guess if needed
     else:
         log10_chi = log10_chi_guess * 1.0
     max_step = 1.0
@@ -136,46 +136,39 @@ def get_ions(sw, salinity, temperature, pressure, rc=None):
     c_ions = (
         np.array(
             [
-                sw["Hfree"].ravel(),
-                sw["OH"].ravel(),
-                rc["Na"].ravel(),
-                rc["Mg"].ravel(),
-                rc["Ca"].ravel(),  # could be from sw?
-                rc["K"].ravel(),
-                rc["Sr"].ravel(),
-                rc["Cl"].ravel(),
-                sw["SO4"].ravel(),
-                sw["HSO4"].ravel(),
-                rc["Br"].ravel(),
-                sw["F"].ravel(),
-                sw["CO3"].ravel(),
-                sw["HCO3"].ravel(),
-                sw["BOH4"].ravel(),
-                sw["H3SiO4"].ravel(),
-                sw["H2PO4"].ravel(),
-                sw["HPO4"].ravel(),
-                sw["PO4"].ravel(),
-                sw["NH4"].ravel(),
-                sw["HS"].ravel(),
+                sw["Hfree"],
+                rc["Na"] + rc["K"] + sw["NH4"],
+                sw["OH"]
+                + rc["Cl"]
+                + rc["Br"]
+                + sw["F"]
+                + sw["HSO4"]
+                + sw["HCO3"]
+                + sw["BOH4"]
+                + sw["H3SiO4"]
+                + sw["H2PO4"]
+                + sw["HS"],
+                rc["Mg"] + rc["Ca"] + rc["Sr"],
+                sw["SO4"] + sw["CO3"] + sw["HPO4"],
+                sw["PO4"],
             ]
         )
         * density
     ).transpose()
-    z_ions = np.array(
-        [1, -1, 1, 2, 2, 1, 2, -1, -2, -1, -1, -1, -2, -1, -1, -1, -1, -2, -3, 1, -1]
-    )  # order must match c_ions!
-    # Enforce charge balance with Na and Cl (because they are probably most abundant)
+    z_ions = np.array([1, 1, -1, 2, -2, 3])  # order must match c_ions!
+    # Enforce charge balance with +1 ions (i.e. Na and Cl, because they are probably
+    # the most abundant)
     cb = np.sum(c_ions * z_ions, axis=1)  # charge balance
     # Where charge balance is negative, add extra Na
-    iNa = 2  # index of column in c_ions containing Na
-    cNa = c_ions[:, iNa]
-    cNa = np.where(cb < 0, cNa - cb, cNa)
-    c_ions[:, iNa] = cNa
+    iCat1 = 1  # index of column in c_ions containing +1 cations (except H+)
+    cCat1 = c_ions[:, iCat1]
+    cCat1 = np.where(cb < 0, cCat1 - cb, cCat1)
+    c_ions[:, iCat1] = cCat1
     # Where charge balance is positive, add extra Cl
-    iCl = 7  # index of column in c_ions containing Cl
-    cCl = c_ions[:, iCl]
-    cCl = np.where(cb > 0, cCl + cb, cCl)
-    c_ions[:, iCl] = cCl
+    iAni1 = 2  # index of column in c_ions containing -1 anions
+    cAni1 = c_ions[:, iAni1]
+    cAni1 = np.where(cb > 0, cAni1 + cb, cAni1)
+    c_ions[:, iAni1] = cAni1
     assert np.all(c_ions >= 0)
     return c_ions, z_ions
 
@@ -184,26 +177,26 @@ def get_ionic_strength(c_ions, z_ions):
     return 0.5 * np.sum(c_ions * z_ions**2, axis=1)
 
 
-def get_dom_bound_protons(
-    total_dom,
-    sw,
-    salinity,
-    temperature,
-    pressure,
-    nd_params,
-    log10_chi_guess=None,
-    niter=100,
-):
-    """Interface function to calculate amount of DOM-bound protons."""
-    c_ions, z_ions = get_ions(sw, salinity, temperature, pressure)
-    ionic_strength = get_ionic_strength(c_ions, z_ions)
-    log10_chi = solve_chi(
-        c_ions,
-        z_ions,
-        ionic_strength,
-        nd_params,
-        log10_chi_guess=log10_chi_guess,
-        niter=niter,
-    )
-    QH = nica(c_ions[:, 0], chi, nd_params)  # mol/kg-DOM
-    return QH * total_dom * 1e-6  # assuming total_dom is in mg-DOM/kg-sw
+# def get_dom_bound_protons(
+#     total_dom,
+#     sw,
+#     salinity,
+#     temperature,
+#     pressure,
+#     nd_params,
+#     log10_chi_guess=None,
+#     niter=100,
+# ):
+#     """Interface function to calculate amount of DOM-bound protons."""
+#     c_ions, z_ions = get_ions(sw, salinity, temperature, pressure)
+#     ionic_strength = get_ionic_strength(c_ions, z_ions)
+#     log10_chi = solve_chi(
+#         c_ions,
+#         z_ions,
+#         ionic_strength,
+#         nd_params,
+#         log10_chi_guess=log10_chi_guess,
+#         niter=niter,
+#     )
+#     QH = nica(c_ions[:, 0], chi, nd_params)  # mol/kg-DOM
+#     return QH * total_dom * 1e-6  # assuming total_dom is in mg-DOM/kg-sw

tests/test_dom.py
@@ -6,50 +6,41 @@ import gsw
 # Test values
 pH = np.linspace(-2, 14, num=100)
 pHstep = np.mean(np.diff(pH))
+# pH = np.array([8.0])
 
-# sw = {}
-# sw["Hfree"] = 10.0**-pH
-# sw["OH"] = 1e-14 / sw["Hfree"]
-# sw["SO4"] = np.full_like(pH, 0.0)
-# sw["HSO4"] = np.full_like(pH, 0.0)
-# sw["F"] = np.full_like(pH, 0.0)
-# sw["CO3"] = np.full_like(pH, 0.0)
-# sw["HCO3"] = np.full_like(pH, 0.0)
-# sw["BOH4"] = np.full_like(pH, 0.0)
-# sw["H3SiO4"] = np.full_like(pH, 0.0)
-# sw["H2PO4"] = np.full_like(pH, 0.0)
-# sw["HPO4"] = np.full_like(pH, 0.0)
-# sw["PO4"] = np.full_like(pH, 0.0)
-# sw["NH4"] = np.full_like(pH, 0.0)
-# sw["HS"] = np.full_like(pH, 0.0)
+sw = {}
+sw["Hfree"] = 10.0**-pH
+sw["OH"] = 1e-14 / sw["Hfree"]
+sw["SO4"] = np.full_like(pH, 0.0)
+sw["HSO4"] = np.full_like(pH, 0.0)
+sw["F"] = np.full_like(pH, 0.0)
+sw["CO3"] = np.full_like(pH, 0.0)
+sw["HCO3"] = np.full_like(pH, 0.0)
+sw["BOH4"] = np.full_like(pH, 0.0)
+sw["H3SiO4"] = np.full_like(pH, 0.0)
+sw["H2PO4"] = np.full_like(pH, 0.0)
+sw["HPO4"] = np.full_like(pH, 0.0)
+sw["PO4"] = np.full_like(pH, 0.0)
+sw["NH4"] = np.full_like(pH, 0.0)
+sw["HS"] = np.full_like(pH, 0.0)
 
-# salinity = np.full_like(pH, 0)
-# temperature = 25
-# pressure = 0
-# rc = None
+salinity = np.full_like(pH, 35.0)
+temperature = 25
+pressure = 0
+rc = None
 
-# c_ions, z_ions = dom.get_ions(sw, salinity, temperature, pressure)
-# ionic_strength = dom.get_ionic_strength(c_ions, z_ions)
-
-#%%
-
-c_H = 10.0**-pH
-c_OH = 1e-14 / c_H
-c_Na = np.full_like(c_H, 1.0)
-c_Ca = np.full_like(c_H, 0.1)
-c_Cl = c_Na + c_H + 2 * c_Ca - c_OH
-assert np.all(c_H + c_Na + 2 * c_Ca - c_OH - c_Cl == 0)
-assert np.all(c_Cl >= 0)
-c_ions = np.array([c_H, c_OH, c_Na, c_Cl, c_Ca]).transpose()
-z_ions = np.array([1, -1, 1, -1, 2])
+c_ions, z_ions = dom.get_ions(sw, salinity, temperature, pressure)
 ionic_strength = dom.get_ionic_strength(c_ions, z_ions)
+c_H = 10**-pH
 
 # Do calculations - fulvic
-log10_chi_fulvic = dom.solve_chi(c_ions, z_ions, ionic_strength, dom.nd_fulvic)
+log10_chi_fulvic = dom.solve_chi(
+    c_ions, z_ions, ionic_strength, dom.nd_fulvic, niter=50
+)
 chi_fulvic = 10.0**log10_chi_fulvic
 Q_H_fulvic = dom.nica(c_H, chi_fulvic, dom.nd_fulvic)
 # Do calculations - humic
-log10_chi_humic = dom.solve_chi(c_ions, z_ions, ionic_strength, dom.nd_humic)
+log10_chi_humic = dom.solve_chi(c_ions, z_ions, ionic_strength, dom.nd_humic, niter=50)
 chi_humic = 10.0**log10_chi_humic
 Q_H_humic = dom.nica(c_H, chi_humic, dom.nd_humic)
 
@@ -73,7 +64,7 @@ ax.plot(pH[1:], -np.diff(Q_H_humic) / pHstep, label="Humic")
 # ax.plot(pH[1:], -np.diff(Q_H_constantchi) / pHstep, label="$\chi$ constant")
 ax.set_xlabel("pH")
 ax.set_ylabel("d$Q_\mathrm{H}$ / dpH")
-ax.legend()
+# ax.legend()
 
 ax = axs[0, 1]
 ax.axhline(0, c="k", lw=0.8)
@@ -110,9 +101,16 @@ ax.set_xlabel("log$_{10}$ $\chi$")
 ax.set_ylabel("Charge balance")
 
 ax = axs[2, 0]
-ax.plot(pH, log10_chi_fulvic)
-ax.plot(pH, log10_chi_humic)
+ax.plot(pH, 10.0**log10_chi_fulvic)
+ax.plot(pH, 10.0**log10_chi_humic)
 ax.set_xlabel("pH")
-ax.set_ylabel("log$_{10}$ $\chi$")
+ax.set_ylabel("$\chi$")
+
+ax = axs[2, 1]
+ax.plot(pH, -np.log10(chi_fulvic * c_H) - pH)
+ax.plot(pH, -np.log10(chi_humic * c_H) - pH)
+# ax.axline((0, 0), slope=1, c="k", lw=0.8)
+ax.set_xlabel("pH")
+ax.set_ylabel("p($\chi c_\mathrm{H}$) $-$ pH")
 
 plt.tight_layout()