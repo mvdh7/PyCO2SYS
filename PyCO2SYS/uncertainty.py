Add opt_pressured_kCO2 to results dict and fix derivatives

PyCO2SYS/uncertainty.py
@@ -413,13 +413,14 @@ def forward_nd(
             if gradable.startswith("k_") and not gradable.endswith("_out")
         ]
     )
-    assert np.all(
-        np.isin(grads_of, gradables)
-    ), "PyCO2SYS error: all grads_of must be in the list at PyCO2SYS.engine.nd.gradables."
+    assert np.all(np.isin(grads_of, gradables)), (
+        "PyCO2SYS error: all grads_of must be in the list at "
+        + "PyCO2SYS.engine.nd.gradables."
+    )
     if np.any([of.endswith("_out") for of in grads_of]):
         assert "temperature_out" in CO2SYS_nd_results, (
-            "PyCO2SYS error: you can only get gradients at output conditions if you calculated"
-            + " results at output conditions!"
+            "PyCO2SYS error: you can only get gradients at output conditions if you "
+            + "calculated results at output conditions!"
         )
     # Extract CO2SYS_nd fixed args from CO2SYS_nd_results and CO2SYS_nd_kwargs.
     # These are arguments that always get a specific value, rather than being calculated
@@ -444,6 +445,7 @@ def forward_nd(
             "opt_pH_scale",
             "opt_total_borate",
             "opt_buffers_mode",
+            "opt_pressured_kCO2",
         ]
         + list(CO2SYS_nd_kwargs.keys())
     )

tests/test_dlnpCO2_dT.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # Simulate arguments
 rng = np.random.default_rng(1)
-npts = 1000
+npts = 10_000
 alkalinity = rng.normal(loc=2300, scale=50, size=npts)
 dic = rng.normal(loc=2150, scale=50, size=npts)
 salinity = rng.uniform(low=0, high=40, size=npts)
@@ -16,6 +16,7 @@ total_phosphate = rng.uniform(low=0, high=100, size=npts)
 total_ammonia = rng.uniform(low=0, high=100, size=npts)
 total_sulfide = rng.uniform(low=0, high=100, size=npts)
 opt_k_carbonic = rng.integers(low=1, high=18, size=npts)
+opt_pressured_kCO2 = rng.integers(low=0, high=2, size=npts)
 
 # Solve
 results = pyco2.sys(
@@ -33,7 +34,7 @@ results = pyco2.sys(
     total_ammonia=total_ammonia,
     total_sulfide=total_sulfide,
     opt_k_carbonic=opt_k_carbonic,
-    opt_pressured_kCO2=0,
+    opt_pressured_kCO2=opt_pressured_kCO2,
     grads_of=["pCO2"],
     grads_wrt=["temperature"],
 )
@@ -51,9 +52,23 @@ max_abs_diff_pct = np.max(abs_diff_pct)
 
 def test_dlnpCO2_dT():
     """Are all of the autograd values the same as the forward finite difference values
-    to within 0.02%?
+    to within:
+        - 0.03% where opt_pressured_kCO2 is 0?
+        - 2.00% where opt_pressured_kCO2 is 1?
+        - 1e-3 everywhere?
+        - 1e-6 in the mean across all inputs?
+        - 0.01% in the mean across all inputs?
     """
-    assert np.allclose(dlnpCO2_dT_autograd, dlnpCO2_dT_ffd, rtol=0.02 / 100, atol=0)
+    L = opt_pressured_kCO2 == 0
+    assert np.allclose(
+        dlnpCO2_dT_autograd[L], dlnpCO2_dT_ffd[L], rtol=0.03 / 100, atol=0
+    )
+    assert np.allclose(
+        dlnpCO2_dT_autograd[~L], dlnpCO2_dT_ffd[~L], rtol=2 / 100, atol=0
+    )
+    assert np.allclose(dlnpCO2_dT_autograd, dlnpCO2_dT_ffd, rtol=0, atol=1e-3)
+    assert np.mean(abs_diff) < 1e-6
+    assert np.mean(abs_diff_pct) < 0.01
 
 
 test_dlnpCO2_dT()