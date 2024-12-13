f2a23e462c1518c62ec4870a511b40a073e5f2a4
Matthew Humphreys <mvdh7@users.noreply.github.com>
2024-12-13 19:18:54 +0100

    Update to 2.0.0

--- PyCO2SYS/meta.py
+++ PyCO2SYS/meta.py
@@ -4,8 +4,8 @@
 import jax
 from jax import numpy as np
 
-version = "1.8.3.1"
-version_xyz = "1.8.3"
+version = "2.0.0"
+version_xyz = "2.0.0"
 authorlist = [
     "Humphreys, Matthew P.",
     "Schiller, Abigail J.",
