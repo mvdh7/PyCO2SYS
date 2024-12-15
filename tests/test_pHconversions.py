60443e4ecaad91bb8f360d2386fb5adc24cede61
Matthew Humphreys <mvdh7@users.noreply.github.com>
2024-12-15 21:27:19 +0100

    Working with CO2System

--- tests/test_pHconversions.py
+++ tests/test_pHconversions.py
@@ -1,47 +1,56 @@
-import numpy as np, PyCO2SYS as pyco2
+import numpy as np
+
+import PyCO2SYS as pyco2
+from PyCO2SYS import CO2System
 
 # Prepare test conditions
 npts = 100
-pHScale = np.full(npts, 1)  # start on Total scale
-pH_T_i = np.random.normal(size=npts, loc=8, scale=1)
+rng = np.random.default_rng(7)
+pH_T_i = rng.normal(size=npts, loc=8, scale=1)
 H_T_i = 10.0**-pH_T_i
 
 # Set and get total molinities
-Sal = np.full(npts, 31.0)
-TSi = np.full(npts, 12.0)
-TPO4 = np.full(npts, 1.5)
-TNH3 = np.full(npts, 2.0)
-TH2S = np.full(npts, 0.5)
-WhichKs = np.full(npts, 10)
-WhoseTB = np.full(npts, 2)
-totals = pyco2.salts.assemble(Sal, TSi, TPO4, TNH3, TH2S, WhichKs, WhoseTB)
-# Set and get equilibrium constants
-TempC = np.full(npts, 22.3)
-Pdbar = np.full(npts, 100.0)
-WhoseKSO4 = np.full(npts, 1)
-WhoseKF = np.full(npts, 1)
-WhichR = np.full(npts, 1)
-equilibria = pyco2.equilibria.assemble(
-    TempC, Pdbar, totals, pHScale, WhichKs, WhoseKSO4, WhoseKF, WhichR
-)
+temperature = 22.3
+salinity = 31.0
+total_sulfate = pyco2.salts.total_sulfate_MR66(salinity)
+total_fluoride = pyco2.salts.total_fluoride_R65(salinity)
+k_HF_free = pyco2.equilibria.p1atm.k_HF_free_PF87(temperature, salinity)
+k_HSO4_free = pyco2.equilibria.p1atm.k_HSO4_free_WM13(temperature, salinity)
+fH = pyco2.convert.fH_TWB82(temperature, salinity)
 
 # Do pH scale conversions in a loop forwards: Total => Seawater => NBS => Free => Total
-H_S_f = H_T_i * pyco2.convert.pH_total_to_sws(totals, equilibria)
-H_N_f = H_S_f * pyco2.convert.pH_sws_to_nbs(totals, equilibria)
-H_F_f = H_N_f * pyco2.convert.pH_nbs_to_free(totals, equilibria)
-H_T_f = H_F_f * pyco2.convert.pH_free_to_total(totals, equilibria)
+H_S_f = H_T_i * pyco2.convert.pH_tot_to_sws(
+    total_fluoride, total_sulfate, k_HF_free, k_HSO4_free
+)
+H_N_f = H_S_f * pyco2.convert.pH_sws_to_nbs(fH)
+H_F_f = H_N_f * pyco2.convert.pH_nbs_to_free(
+    total_fluoride, total_sulfate, k_HF_free, k_HSO4_free, fH
+)
+H_T_f = H_F_f * pyco2.convert.pH_free_to_tot(total_sulfate, k_HSO4_free)
 
 # Do pH scale conversions in a loop backwards: Total => Free => NBS => Seawater => Total
-H_F_b = H_T_i * pyco2.convert.pH_total_to_free(totals, equilibria)
-H_N_b = H_F_b * pyco2.convert.pH_free_to_nbs(totals, equilibria)
-H_S_b = H_N_b * pyco2.convert.pH_nbs_to_sws(totals, equilibria)
-H_T_b = H_S_b * pyco2.convert.pH_sws_to_total(totals, equilibria)
+H_F_b = H_T_i * pyco2.convert.pH_tot_to_free(total_sulfate, k_HSO4_free)
+H_N_b = H_F_b * pyco2.convert.pH_free_to_nbs(
+    total_fluoride, total_sulfate, k_HF_free, k_HSO4_free, fH
+)
+H_S_b = H_N_b * pyco2.convert.pH_nbs_to_sws(fH)
+H_T_b = H_S_b * pyco2.convert.pH_sws_to_tot(
+    total_fluoride, total_sulfate, k_HF_free, k_HSO4_free
+)
 
 # Do the missing combinations
-H_N_m = H_T_i * pyco2.convert.pH_total_to_nbs(totals, equilibria)
-H_T_m = H_N_m * pyco2.convert.pH_nbs_to_total(totals, equilibria)
-H_S_m = H_F_b * pyco2.convert.pH_free_to_sws(totals, equilibria)
-H_F_m = H_S_f * pyco2.convert.pH_sws_to_free(totals, equilibria)
+H_N_m = H_T_i * pyco2.convert.pH_tot_to_nbs(
+    total_fluoride, total_sulfate, k_HF_free, k_HSO4_free, fH
+)
+H_T_m = H_N_m * pyco2.convert.pH_nbs_to_tot(
+    total_fluoride, total_sulfate, k_HF_free, k_HSO4_free, fH
+)
+H_S_m = H_F_b * pyco2.convert.pH_free_to_sws(
+    total_fluoride, total_sulfate, k_HF_free, k_HSO4_free
+)
+H_F_m = H_S_f * pyco2.convert.pH_sws_to_free(
+    total_fluoride, total_sulfate, k_HF_free, k_HSO4_free
+)
 
 
 def test_pH_conversions():
@@ -57,14 +66,25 @@ def test_pH_conversions():
 
 
 def test_pH_conversions_sys():
-    r1 = pyco2.sys(par1=8.1, par1_type=3, opt_pH_scale=1)
-    r2 = pyco2.sys(par1=r1["pH_sws"], par1_type=3, opt_pH_scale=2)
-    r3 = pyco2.sys(par1=r1["pH_free"], par1_type=3, opt_pH_scale=3)
-    r4 = pyco2.sys(par1=r1["pH_nbs"], par1_type=3, opt_pH_scale=4)
+    sys1 = CO2System(values=dict(pH=8.1), opts=dict(opt_pH_scale=1))
+    sys1.solve(["pH_sws", "pH_free", "pH_nbs"])
+    sys1.values["pH_total"] = sys1.values["pH"]
+    sys2 = CO2System(values=dict(pH=sys1.values["pH_sws"]), opts=dict(opt_pH_scale=2))
+    sys2.solve(["pH_total", "pH_free", "pH_nbs"])
+    sys2.values["pH_sws"] = sys2.values["pH"]
+    sys3 = CO2System(values=dict(pH=sys1.values["pH_free"]), opts=dict(opt_pH_scale=3))
+    sys3.solve(["pH_total", "pH_sws", "pH_nbs"])
+    sys3.values["pH_free"] = sys3.values["pH"]
+    sys4 = CO2System(values=dict(pH=sys1.values["pH_nbs"]), opts=dict(opt_pH_scale=4))
+    sys4.solve(["pH_total", "pH_sws", "pH_free"])
+    sys4.values["pH_nbs"] = sys4.values["pH"]
     for scale in ["pH_total", "pH_sws", "pH_free", "pH_nbs"]:
-        assert np.isclose(r1[scale], r2[scale], rtol=0, atol=1e-12)
-        assert np.isclose(r2[scale], r3[scale], rtol=0, atol=1e-12)
-        assert np.isclose(r3[scale], r4[scale], rtol=0, atol=1e-12)
+        assert np.isclose(sys1.values[scale], sys2.values[scale], rtol=0, atol=1e-12)
+        assert np.isclose(sys1.values[scale], sys3.values[scale], rtol=0, atol=1e-12)
+        assert np.isclose(sys1.values[scale], sys4.values[scale], rtol=0, atol=1e-12)
+        assert np.isclose(sys2.values[scale], sys3.values[scale], rtol=0, atol=1e-12)
+        assert np.isclose(sys2.values[scale], sys4.values[scale], rtol=0, atol=1e-12)
+        assert np.isclose(sys3.values[scale], sys4.values[scale], rtol=0, atol=1e-12)
 
 
 test_pH_conversions()
