ea94b3547ed8660adb823ae39843b9b32d67edca
Matthew Humphreys <mvdh7@users.noreply.github.com>
2025-02-13 12:32:21 +0100

    Improve validity ranges

--- tests/_test_valid.py
+++ tests/_test_valid.py
@@ -5,24 +5,24 @@ from matplotlib import pyplot as plt
 
 import PyCO2SYS as pyco2
 
-co2s = pyco2.sys(dic=2200, alkalinity=2300, salinity=30, temperature=25)
-co2s.solve(["k_HCO3_sws_1atm"], store_steps=2)
+co2s = pyco2.sys(dic=2200, pCO2=400, salinity=35, temperature=30)
+co2s.solve(["k_CO2", "k_H2CO3", "k_HCO3"], store_steps=2)
 # co2s.solve(["pH"], store_steps=2)
 co2s.check_valid(
     # ignore="total_borate",
 )
 
 # Visualise
-fig, ax = plt.subplots(figsize=(6, 6), dpi=300)
+fig, ax = plt.subplots(figsize=(8, 6), dpi=300)
 co2s.plot_graph(
     ax=ax,
     # prog_graphviz="sfdp",
     prog_graphviz="dot",
     show_unknown=False,
     show_isolated=False,
-    # skip_nodes=["gas_constant", "pressure_atmosphere"],
+    skip_nodes=["gas_constant", "ionic_strength"],
     mode="valid",
-    node_kwargs={"node_size": 1500},
-    edge_kwargs={"node_size": 1500},
-    label_kwargs={"font_size": 12},
+    node_kwargs={"node_size": 650, "alpha": 0.8},
+    edge_kwargs={"node_size": 650},
+    label_kwargs={"font_size": 8},
 )
