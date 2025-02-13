d92319b6ecffd59adc36ddaf986428994c199cee
Matthew Humphreys <mvdh7@users.noreply.github.com>
2025-02-13 11:14:17 +0100

    Updates

--- tests/_test_valid.py
+++ tests/_test_valid.py
@@ -1,19 +1,23 @@
 # %%
 import networkx as nx
 import numpy as np
+from matplotlib import pyplot as plt
 
 import PyCO2SYS as pyco2
 
-co2s = pyco2.sys(salinity=42)
-co2s.solve(["k_HSO4_free", "k_CO2", "k_H2CO3"], store_steps=2)
+co2s = pyco2.sys(dic=2200, alkalinity=2300)
+co2s.solve(["pH"], store_steps=2)
 
 # Visualise
+fig, ax = plt.subplots(figsize=(12, 12))
 co2s.plot_graph(
-    prog_graphviz="dot",
-    show_unknown=False,
-    show_isolated=False,
+    ax=ax,
+    prog_graphviz="sfdp",
+    show_unknown=True,
+    show_isolated=True,
+    # skip_nodes=["gas_constant", "pressure_atmosphere"],
     mode="valid",
-    node_kwargs={"node_size": 700},
-    edge_kwargs={"node_size": 700},
+    node_kwargs={"node_size": 600},
+    edge_kwargs={"node_size": 600},
     label_kwargs={"font_size": 9},
 )
