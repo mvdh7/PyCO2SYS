60640d78dbcfee3135cbf5e8aa84ec03f802d0d9
Matthew Humphreys <mvdh7@users.noreply.github.com>
2025-04-24 09:05:01 +0200

    Remove defunct function

--- PyCO2SYS/constants.py
+++ PyCO2SYS/constants.py
@@ -11,12 +11,3 @@ RGasConstant_CODATA2018 = 83.14462618  # 10^-1 J mol^-1 K^-1 (available from v1.
 # RGasConstant_CODATA2018 added in v1.4.1 for consistency with CO2SYS-MATLAB v3,
 # but the default remains RGasConstant_DOEv2.
 Tzero = 273.15  # 0 degC in K
-
-
-def RGasConstant(WhichR):
-    """Return the gas constant R in ml / (bar * K * mol)."""
-    RGas = np.full(np.shape(WhichR), np.nan)
-    RGas = np.where(WhichR == 1, RGasConstant_DOEv2, RGas)  # default, DOEv2
-    RGas = np.where(WhichR == 2, RGasConstant_DOEv3, RGas)  # DOEv3
-    RGas = np.where(WhichR == 3, RGasConstant_CODATA2018, RGas)  # 2018 CODATA
-    return RGas
