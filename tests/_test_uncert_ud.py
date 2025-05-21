f9cae8379c011161bcbdcd983a959f2992fc7a3b
Matthew Humphreys <mvdh7@users.noreply.github.com>
2025-05-21 15:00:34 +0200

    Add noqa

--- tests/_test_uncert_ud.py
+++ tests/_test_uncert_ud.py
@@ -38,8 +38,8 @@ uncert.components["pH"] = DotDict(dic=0.03, temperature=0.04)
 uncert["pH"] = 0.05
 
 # Check if it works
-co2s = pyco2.sys(dic=2100, fco2=400).set_uncertainty(t=1, s=1).propagate("ph")
+co2s = pyco2.sys(dic=2100, fco2=400).set_u(t=1, s=1).propagate("ph")
 co2a = co2s.adjust(t=10)  # .propagate()
-co2s.requested
+co2s.requested  # noqa
 # set_uncertainty(t=1, s=1).propagate("ph")
 # co2a = co2s.adjust(t=10).propagate()
