0f45c067aa8efd5c2ef7f6164fedab7b38280940
Matthew Humphreys <mvdh7@users.noreply.github.com>
2025-07-21 15:30:50 +0200

    Bug fixed

--- tests/_test_Mg_calcite_v2.py
+++ tests/_test_Mg_calcite_v2.py
@@ -9,9 +9,7 @@ co2s = pyco2.sys(
     opt_Mg_calcite_kt_Tdep=3,
     mg_fraction=0.18,
 ).solve(["pkt_Mg_calcite_25C_1atm", "saturation_Mg_calcite"], store_steps=2)
-# BUG if explicitly ask to solve a non-standard stored param then it
-# should actually be stored even with store_steps != 2
-# print(co2s.pkt_Mg_calcite_25C_1atm)
+print(co2s.pkt_Mg_calcite_25C_1atm)
 print(co2s["saturation_mg_calcite"])
 print(co2s["oc"])
 print(co2s["oa"])
